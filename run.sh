3f1eb3dcf06d077d7056bd927c34affdcfda265b
Chris Cates <hello@chriscates.ca>
2018-06-05 21:58:48 -0400

    Updated shell script

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -v
 
 # Run both scripts parallel
-# python src/game.py & python src/services/PavlokServer.py && fg
+python src/game.py & python src/services/PavlokServer.py && fg
 
-python src/services/PavlokServer.py
+# Can comment the above and comment either/or for debugging purposes
+# python src/services/PavlokServer.py
+# python src/game.py
